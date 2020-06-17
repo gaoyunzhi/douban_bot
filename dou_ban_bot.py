b9453d678c1f13efdf2e141dbf97ea9eeb7363fe
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-06-17 08:48:27 -0700

    commit

--- dou_ban_bot.py
+++ dou_ban_bot.py
@@ -6,8 +6,6 @@ from telegram.ext import Updater, MessageHandler, Filters
 import export_to_telegraph
 import time
 import yaml
-import web_2_album
-import album_sender
 from bs4 import BeautifulSoup
 import cached_url
 from db import DB
@@ -22,93 +20,39 @@ debug_group = tele.bot.get_chat(420074357)
 
 db = DB()
 
-def getSource(item):
-	new_status = item
-	while 'new-status' not in new_status.get('class'):
-		new_status = new_status.parent
-	if new_status.get('data-sid'):
-		return 'https://www.douban.com/people/%s/status/%s/' % \
-			(new_status['data-uid'], new_status['data-sid'])
-
-def getCap(quote, url):
-	if '_' in url:
-		url = '[%s](%s)' % (url, url)
-	return cutCaption(quote, url, 4000)
-
-def getResult(post_link, item):
-	raw_quote = item.find('blockquote') or ''
-	quote = export_to_telegraph.exportAllInText(raw_quote)
-	quote = compactText(quote).strip('更多转发...').strip()
-
-	r = web_2_album.Result()
-
-	note = item.find('div', class_='note-block')
-	if (note and note.get('data-url')) or matchKey(post_link, 
-			['https://book.douban.com/review/', 'https://www.douban.com/note/']):
-		note = (note and note.get('data-url')) or post_link
-		url = export_to_telegraph.export(note, force=True) or note
-		r.cap = getCap(quote, url)
-		return r
-
-	if item.find('div', class_='url-block'):
-		url = item.find('div', class_='url-block')
-		url = url.find('a')['href']
-		url = clearUrl(export_to_telegraph.export(url) or url)
-		r.cap = getCap(quote, url)
-		return r
-
-	if '/status/' in post_link:
-		r = web_2_album.get(post_link, force_cache=True)
-		r.cap = quote
-		if r.imgs:
-			return r
-
-	if quote and raw_quote.find('a', title=True, href=True):
-		r.cap = quote
-		return r
-
-def process(item, channels):
-	try:
-		post_link = item.find('span', class_='created_at').find('a')['href']
-	except:
+def process(note, channels):
+	if db.existing.add(note):
 		return
-	source = getSource(item) or post_link
-
-	if db.existing.add(source) or db.existing.add(post_link):
-		return
-
-	result = getResult(post_link, item)
-	if not result:
-		return
-
+	note = export_to_telegraph.export(note, force=True) or note
 	for channel in channels:
-		time.sleep(20)
-		album_sender.send(channel, source, result)
+		channel.send_message(note)
 
+def getNotes(user_id, page=1):
+	url = 'https://www.douban.com/people/%s/notes?p=%d' % (user_id, page)
+	soup = BeautifulSoup(cached_url.get(url, sleep=20), 'html.parser')
+	for item in soup.find_all('div', class_="note-container"):
+		yield item['data-url']
+		
 @log_on_fail(debug_group)
 def loopImp():
 	removeOldFiles('tmp', day=0.1)
 	for user_id in db.sub.subscriptions():
 		channels = db.sub.channels(user_id)
-		url = 'https://www.douban.com/people/%s/statuses' % user_id
-		soup = BeautifulSoup(cached_url.get(url, sleep=20), 'html.parser')
-		for item in soup.find_all('div', class_='status-item'):
-			process(item, channels)
+		for note in getNotes(user_id)
+			process(note, channels)
 
 def backfill(chat_id):
 	channels = [tele.bot.get_chat(chat_id)]
 	for user_id in db.sub.sub.get(chat_id, []):
-		url_prefix = 'https://www.douban.com/people/%s/statuses' % user_id
 		page = 0
 		while True:
 			page += 1
-			url = url_prefix + '?p=' + str(page)
-			soup = BeautifulSoup(cached_url.get(url_prefix, sleep=20), 'html.parser')
-			items = list(soup.find_all('div', class_='status-item'))
-			if not items:
+			notes = list(getNotes(user_id, page))
+			if not notes:
 				return
-			for item in items:
+			for note in notes:
 				process(item, channels)
+	tele.bot.get_chat(chat_id).send_message('finished backfill')
 
 def doubanLoop():
 	loopImp()
@@ -130,6 +74,8 @@ def handleCommand(update, context):
 		parse_mode='markdown', disable_web_page_preview=True)
 
 HELP_MESSAGE = '''
+** Currently support notes only **
+
 Commands:
 /dbb_add - add douban user
 /dbb_remove - remove douban user
